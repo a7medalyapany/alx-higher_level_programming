dd69879f20a361ec21a33aab6b304d71bf5778df
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-09-27 04:16:14 +0300

    initial

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""This module defines a Square class"""
 
 
 class Square:
+    """This class is a square with a size"""
 
     def __init__(self, size=0):
+        """Initialize a new Square with a size
+
+        Args: size (int): The size of the new square.
+        """
         self.__size = size
 
     def area(self):
+        """Return the area of this square"""
         return (self.__size * self.__size)
