96bcbd56ee74de250db2804de4cbedb52cc90412
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-09-27 04:20:17 +0300

    initial

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,50 +1,58 @@
 #!/usr/bin/python3
+"""This module defines a Square class"""
+
+
 class Square:
+    """This class is a square with a size"""
 
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if not isinstance(position, tuple) or len(position) != 2 \
-                or not all(isinstance(i, int) and i >= 0 for i in position):
-            raise TypeError(
-                "position must be a tuple of two positive integers")
+        """Initialize a new Square with a size
+
+        Args:
+                        size (int): The size of the new square.
+        """
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
+        """int: The value of __size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+        """Set __size to a value
+
+        Args:
+                        value (int): The value to set __size to.
+        """
         self.__size = value
 
     @property
     def position(self):
+        """tuple: The value of __position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 \
-                or not all(isinstance(i, int) and i >= 0 for i in value):
-            raise TypeError(
-                "position must be a tuple of two positive integers")
-        self.__position = value
+     """Set __position to a value
 
-    def area(self):
-        return self.__size * self.__size
+		Args:
+						value (tuple): The value to set __position to.
+		"""
+		self.__position = value
 
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+		def area(self):
+			"""Return the area of this square"""
+			return (self.__size * self.__size)
+
+				def my_print(self):
+					"""Print this square"""
+					if self.__size == 0:
+						print()
+					else:
+						for i in range(self.__position[1]):
+							print()
+						for i in range(self.__size):
+							print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+
