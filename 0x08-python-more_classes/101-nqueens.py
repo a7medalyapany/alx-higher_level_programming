4d1905de7f697d3cd5e218e571ba31640ea4ca8b
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-10-02 19:14:37 +0300

    initial

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,23 +1,65 @@
 #!/usr/bin/python3
 
-"""Module for solving the N queens puzzle.
-Holberton School
-Foundations - Higher-level programming - Python
-By Stuart Echeverry
-"""
 
 import sys
 
 
-def printBoard(board):
-    """Print the board positions
-    """
+def is_safe(board, row, col, N):
+    """Check if it's safe to place a queen at board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
 
-    solution = []
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 1:
-                solution.append([i, j])
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-    print(solution)
+    return True
+
+
+def solve_nqueens(N):
+    """Solve the N queens puzzle using backtracking"""
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+
+    def solve_col(board, col, N):
+        if col >= N:
+            solution = []
+            for row in range(N):
+                for col in range(N):
+                    if board[row][col] == 1:
+                        solution.append([row, col])
+            solutions.append(solution)
+            return
+
+        for row in range(N):
+            if is_safe(board, row, col, N):
+                board[row][col] = 1
+                solve_col(board, col + 1, N)
+                board[row][col] = 0
+
+    solve_col(board, 0, N)
+
+    return solutions
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
