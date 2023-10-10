f626d2afeadcca9435739c30927feff5dc25956b
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-10-10 19:57:18 +0300

    final

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Function that writes an Object to a text file, using a JSON representation"""
+"""Function that writes an Object to a text file,
+using a JSON representation"""
 import json
 
 
