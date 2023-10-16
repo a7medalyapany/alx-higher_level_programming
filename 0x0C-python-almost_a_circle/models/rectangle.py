e156a753a616d9896bd77231e47ed4cca79df5a9
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-10-16 18:38:46 +0300

    initial

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,23 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y,
             self.__width, self.__height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        """Internal update method"""
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Update method"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
