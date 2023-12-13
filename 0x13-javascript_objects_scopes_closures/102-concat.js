38d301d62da2837d387969e537657e51e1b0858f
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-12-13 19:18:06 +0000

    final

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,26 +2,23 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.error('Usage: node 102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
+const [, , fileA, fileB, fileC] = process.argv;
 
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-
+// Read contents of fileA
 fs.readFile(fileA, 'utf8', (err, dataA) => {
   if (err) throw err;
 
+  // Read contents of fileB
   fs.readFile(fileB, 'utf8', (err, dataB) => {
     if (err) throw err;
 
-    const concatenatedContent = dataA + '\n' + dataB;
+    // Concatenate data from fileA and fileB
+    const concatenatedData = `${dataA}${dataB}`;
 
-    fs.writeFile(fileC, concatenatedContent, 'utf8', (err) => {
+    // Write concatenated data to fileC
+    fs.writeFile(fileC, concatenatedData, (err) => {
       if (err) throw err;
-      console.log('The files have been concatenated and saved to', fileC);
+      console.log(`The content of ${fileA} and ${fileB} has been concatenated and saved to ${fileC}`);
     });
   });
 });
