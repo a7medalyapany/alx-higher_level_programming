fd9a66aaac5bc57ed4c2950eebc8e1ed2de2515a
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-02-20 22:52:02 +0200

    style: spaces

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -18,7 +18,10 @@ request(apiUrl, (error, response, body) => {
 
   const films = JSON.parse(body).results;
   const numMovies = films.reduce((count, film) => {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+    if (
+      film.characters.includes(`https://swapi-api.alx-tools.com/
+	                            api/people/${characterId}/`)
+    ) {
       return count + 1;
     }
     return count;
