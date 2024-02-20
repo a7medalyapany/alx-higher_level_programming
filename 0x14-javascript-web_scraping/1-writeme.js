4898c7701ecdcd32ef0ff20d89c97d267616f370
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-02-20 22:56:45 +0200

    initial

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const content = process.argv[3];
+function writeFile(filePath, content) {
+  fs.writeFile(filePath, content, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`${content} has been written to ${filePath}`);
-});
+if (process.argv.length === 4) {
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeFile(filePath, content);
+}
